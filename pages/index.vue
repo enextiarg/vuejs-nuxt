8981d60ea724a4ffcce544b454497b7023871dc1
Fabián Delgado <fabiandelgado@rexolution.com>
2017-08-19 20:41:30 -0300

    Bug fix api

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,33 @@
 <template>
-  <div>
-    <div class="Hero Hero--center">
-      <div class="Hero-cover">
-        <div class="Crop">
-          <img src="http://1.bp.blogspot.com/-S-pkqiT-87k/UdHWC7dhDrI/AAAAAAAALb0/9fdtV9-3z4E/s1600/plazaIndependencia.jpg" alt="">
-        </div>
-      </div>
-      <div class="Hero-body">
-        <div class="Container">
-          <div class="Hero-title">
-            Drupal Decoupled
+<div>
+  <div class="Grid-item u-md-size2of3">
+  <div class="Box Box--destacado1">
+    <div class="Media Media--rev">
+      <div class="Media-image">
+        <img v-bind:src="noticias[0].field_fotos[0].url" alt="" width="260px" height="260px">
           </div>
-          <div class="Hero-description">
-            Nuxtjs + Vuejs + Webpack + Drupal WS
-          </div>
+              <div class="Media-body">
+                <span class="Box-info">{{ noticias[0].field_fecha[0].value}}</span>
+                <h3 class="Box-title">
+                 <a href="">{{ noticias[0].title[0].value }}</a>
+                </h3>
+               <p class="Box-text">{{noticias[0].field_resumen[0].value}}</p>
+              </div>
         </div>
-      </div>
     </div>
-    <noticias/>
   </div>
+</div>
 </template>
-
 <script>
-import Noticias from '~/plugins/Noticias.vue'
+import axios from 'axios'
+
 export default {
-  components: {
-    Noticias
+  asyncData ({ req, params }) {
+    // We can return a Promise instead of calling the callback
+    return axios.get('http://dev-rexolution.pantheonsite.io/api/noticias')
+      .then((res) => {
+        return { noticias: res.data }
+      })
   }
 }
 </script>
--- store/nav.js
+++
@@ -1,6 +0,0 @@
-export const actions = {
-  async fetchNavItems (state) {
-    let res = await Axios.get('https://cosmicjs.com/v1/marcocardoso/object-type/pages')
-    state.commit('setNavItems', res.data.objects)
-  }
-};
